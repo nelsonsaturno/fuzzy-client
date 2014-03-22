8c03c8ed08403349ad8bcb5f167f47e1593f3a03
José Alberto Goncalves Da Silva <gml.josea2@gmail.com>
2014-03-22 12:01:50 -0430

    Migré el script para el catalogo de datos Tipo 2 para que funcione en Postgres.
    Una 'database' en Postgres es distinta a una en MySQL/MariaDB. El concepto
    más cercano es de 'schema', así que lo cambié a eso.
    
    INTEGER AUTO_INCREMENT no existe en Postgres, el equivalente es SERIAL.
    
    La sintaxis UNIQUE KEY (c1, c2, ...) no sirve en Postgres, le quité el
    KEY para que funcionara.

--- querying-based-on-similarity/src/sql_scripts/create-schema.sql
+++ querying-based-on-similarity/src/sql_scripts/create-schema.sql
@@ -1,17 +1,17 @@
-CREATE DATABASE IF NOT EXISTS information_schema_fuzzy CHARACTER SET = utf8;
+CREATE SCHEMA IF NOT EXISTS information_schema_fuzzy; -- CHARACTER SET = utf8;
 
 CREATE TABLE IF NOT EXISTS information_schema_fuzzy.domains (
-  domain_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  domain_id SERIAL PRIMARY KEY,
   table_schema VARCHAR(64) NOT NULL,
   domain_name VARCHAR(64) NOT NULL,
-  UNIQUE KEY (table_schema, domain_name)
+  UNIQUE (table_schema, domain_name)
 );
 
 CREATE TABLE IF NOT EXISTS information_schema_fuzzy.labels (
-  label_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  label_id SERIAL PRIMARY KEY,
   domain_id INTEGER NOT NULL,
   label_name VARCHAR(64) NOT NULL,
-  UNIQUE KEY (domain_id, label_name),
+  UNIQUE (domain_id, label_name),
   FOREIGN KEY (domain_id) REFERENCES information_schema_fuzzy.domains(domain_id) 
     ON UPDATE CASCADE ON DELETE CASCADE
 );
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS information_schema_fuzzy.labels (
 CREATE TABLE IF NOT EXISTS information_schema_fuzzy.similarities (
   label1_id INTEGER NOT NULL,
   label2_id INTEGER NOT NULL,
-  value DECIMAL(31,30) UNSIGNED NOT NULL,
+  value DECIMAL(31,30) NOT NULL,
   derivated BOOLEAN NOT NULL DEFAULT false,
   PRIMARY KEY (label1_id, label2_id),
   FOREIGN KEY (label1_id)  REFERENCES information_schema_fuzzy.labels (label_id) 
